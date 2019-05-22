aba02e21fb99c192b4ef58aec974ee0df1c189a9
Sukanya <sukanya.c.mohan@gmail.com>
2019-05-22 15:51:32 -0400

    initial commit

---
+++ app/src/main/java/com/example/activity_login/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.activity_login;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private EditText Name;
+    private EditText Password;
+    private Button Login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Name = (EditText)findViewById(R.id.etName);
+        Password = (EditText)findViewById(R.id.etPassword);
+        Login = (Button) findViewById(R.id.btnLogin);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               validate(Name.getText().toString(), Password.getText().toString());
+            }
+        });
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void validate(String userName, String userPassword){
+        if((userName.equals("student@gc")) && (userPassword .equals("1234"))){
+            Snackbar.make(findViewById(R.id.root), "Welcome", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+
+        }
+        else{
+            Snackbar.make(findViewById(R.id.root), "Try agian", Snackbar.LENGTH_LONG)
+                   .setAction("Action", null).show();
+
+
+        }
+    }
+
+
+
+
+}
+
+
+
